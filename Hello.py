adc37ab8cb4264500fd9881d363439e082d672e7
Luciano Leguizamon <lucianoleguizamon@outlook.com>
2024-03-10 16:23:05 +0000

    t13r1

--- Hello.py
+++ Hello.py
@@ -1,37 +1,37 @@
-# import hmac
-# import hashlib
-# import requests
-# import json
-# import time
-# import datetime
-# import pandas as pd
+import hmac
+import hashlib
+import requests
+import json
+import time
+import datetime
+import pandas as pd
 import streamlit as st
-# import altair as alt
-# import sys
-# import numpy as np
+import altair as alt
+import sys
+import numpy as np
 
-# st.set_page_config(
-#     page_title="Interés compuesto | Compound interest",
-#     page_icon="📈",
-#     layout="wide",
-#     initial_sidebar_state="expanded",
-#     menu_items={"About":""}
-# )
+st.set_page_config(
+    page_title="Interés compuesto | Compound interest",
+    page_icon="📈",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={"About":""}
+)
 
-# alt.themes.enable("dark")
+alt.themes.enable("dark")
 
-# uri = "https://api.binance.com"
+uri = "https://api.binance.com"
 
-# ##  Fill in your Binance API key and Secret keys:
-# binance_api_key = st.secrets["KEY"]
-# binance_api_secret = st.secrets["SECRET"]
+##  Fill in your Binance API key and Secret keys:
+binance_api_key = st.secrets["KEY"]
+binance_api_secret = st.secrets["SECRET"]
 
-# def get_timestamp_offset():
-#     url = "{}/api/v3/time".format(uri)
-#     payload = {}
-#     headers = {"Content-Type": "application/json"}
-#     response = requests.request("GET", url, headers=headers, data=payload)
-#     result = json.loads(response.text)["serverTime"]-int(time.time()*1000)
-#     return result
+def get_timestamp_offset():
+    url = "{}/api/v3/time".format(uri)
+    payload = {}
+    headers = {"Content-Type": "application/json"}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    result = json.loads(response.text)["serverTime"]-int(time.time()*1000)
+    return result
 st.text("gg")
 
