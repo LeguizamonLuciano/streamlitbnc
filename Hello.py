5831713173d6358f4227e758289fb4a94c0fcdb8
Luciano Leguizamon <lucianoleguizamon@outlook.com>
2024-03-10 20:17:51 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -39,8 +39,8 @@ def compound_interest(principal, annual_rate, years, monthly_contribution):
 uri = "https://api.binance.com"
 
 ##  Fill in your Binance API key and Secret keys:
-binance_api_key = "KEY"
-binance_api_secret = "SECRET"
+binance_api_key = KEY
+binance_api_secret = SECRET
 
 def get_timestamp_offset():
     url = "{}/api/v3/time".format(uri)
